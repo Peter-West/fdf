e9e6d58c56ce8e1cf34874fe4b9a463531d1b3d2
Pierre PELLEGRINI <ppellegr@e2r9p5.42.fr>
2017-02-21 18:26:18 +0100

    second commit

--- incs/fdf.h
+++ incs/fdf.h
@@ -1,6 +1,9 @@
 #ifndef FDF_H
 # define FDF_H
 # define SPACE_INBETWEEN	30
+# define COLOR_UP			16714390
+# define COLOR_DOWN			3010730
+# define COLOR_STEP			(16714390 - 3010730) / 30
 # include "libft.h"
 # include "get_next_line.h"
 # include <mlx.h>
@@ -17,14 +20,15 @@ typedef struct		s_point
 typedef struct		s_env
 {
 	t_point			*points;
-	void	*mlx_ptr;
-	void	*img_ptr;
-	void	*win_ptr;
+	void			*mlx_ptr;
+	void			*img_ptr;
+	void			*win_ptr;
+	int				z_min;
+	int				z_max;
 }					t_env;
 
 void		fill_list(t_point **points, t_point *point);
 void		read_map(t_env *e, char *name);
 void		fill_struct(t_point **points, char *line);
-
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -25,6 +25,23 @@ void		fill_struct(t_point **points, char *line)
 	y++;
 }
 
+void		min_max(t_env *e)
+{
+	t_point	*tmp;
+
+	tmp = e->points;
+	e->z_min = 0;
+	e->z_max = 0;
+	while (tmp)
+	{
+		if (tmp->val < e->z_min)
+			tmp->val = e->z_min;
+		if (tmp->val < e->z_max)
+			tmp->val = e->z_max;
+		tmp = tmp->next;
+	}
+}
+
 void		fill_list(t_point **points, t_point *point)
 {
 	t_point	*tmp;
@@ -39,7 +56,6 @@ void		fill_list(t_point **points, t_point *point)
 		while (tmp->next)
 			tmp = tmp->next;
 		tmp->next = new_p;
-
 	}
 }
 
@@ -48,7 +64,11 @@ void		read_map(t_env *e, char *name)
 	char	*line;
 	int		fd;
 
-	fd = open(name, O_RDONLY);
+	if (!(fd = open(name, O_RDONLY)))
+	{
+		ft_putstr("Can't open file\n");
+		exit(1);
+	}
 	while (get_next_line(fd, &line))
 	{
 		fill_struct(&e->points, line);
@@ -71,6 +91,43 @@ int			key_hook(int keycode, void *param)
 	return (0);
 }
 
+int			color_change(int val_start, int val_end)
+{
+	if (val_start == val_end)
+		return (0);
+	if (val_start > 0 && val_end > 0)
+		return (COLOR_UP);
+	if (val_start > 0 && val_end < 0)
+
+	if (val_start > 0 && val_end == 0)
+	
+	if (val_start < 0 && val_end == 0)
+
+	if (val_start < 0 && val_end > 0)
+
+	if (val_start < 0 && val_end < 0)
+		return (COLOR_DOWN);
+	
+
+		return ((COLOR_BASE - COLOR_UP) / SPACE_INBETWEEN);
+	if (val < 0)
+	{
+		// printf("check : %d\n", (COLOR_BASE - COLOR_DOWN) / SPACE_INBETWEEN);
+		return ((COLOR_BASE - COLOR_DOWN) / SPACE_INBETWEEN);
+	}
+	return (0);
+}
+
+int		color_start(t_env *e, int z_val, int step)
+{
+	/*if (val_start == e->z_max)
+		return (COLOR_UP);
+	if (val_start == e->z_min)
+		return (COLOR_DOWN);
+	*/
+	return (COLOR_DOWN + (COLOR_STEP * (z_val - val_curr)));
+}
+
 void		draw_lines(t_env *e)
 {
 	t_point	*tmp;
@@ -78,14 +135,20 @@ void		draw_lines(t_env *e)
 	int		color;
 
 	tmp = e->points;
-	color = 3010730;
 	while (tmp)
 	{
 		if (tmp->next && tmp->next->x == tmp->x + SPACE_INBETWEEN)
 		{
+			color = color_start(e, tmp->val);
 			x = tmp->x;
 			while (x < tmp->next->x)
+			{
+				printf("color : %d\n", color);
+				// color -= color_change(tmp->val, tmp->next->val);
+				color = color_start(e, tmp->val);
+
 				mlx_pixel_put (e->mlx_ptr, e->win_ptr, x++, tmp->y , color);
+			}
 		}
 		tmp = tmp->next;
 	}
@@ -94,18 +157,27 @@ void		draw_lines(t_env *e)
 void		draw_columns(t_env *e)
 {
 	t_point	*tmp;
+	t_point	*tmp2;
 	int		y;
 	int		color;
 
 	tmp = e->points;
-	color = 3010730;
 	while (tmp)
 	{
-		if (tmp->next && tmp->next->y == tmp->y + SPACE_INBETWEEN)
+		tmp2 = e->points;
+		while(tmp2)
 		{
-			y = tmp->y;
-			while (y < tmp->next->y)
-				mlx_pixel_put (e->mlx_ptr, e->win_ptr, tmp->x, y++, color);
+			if (tmp->x == tmp2->x && tmp->y + SPACE_INBETWEEN == tmp2->y)
+			{
+				y = tmp->y;
+				color = color_start(e, tmp->val);
+				while (y < tmp2->y)
+				{
+					color -= color_change(tmp->val, tmp2->val);
+					mlx_pixel_put (e->mlx_ptr, e->win_ptr, tmp->x, y++, color);
+				}
+			}
+			tmp2 = tmp2->next;
 		}
 		tmp = tmp->next;
 	}
@@ -158,6 +230,8 @@ int			main(int argc, char **argv)
 	}
 	read_map(&e, argv[1]);
 	// debug(&e);
+	min_max(&e);
+	printf("z_min : %d, z_max : %d\n", e.z_min, e.z_max);
 	init_mlx(&e);
 	loop(&e);
 	return (0);
